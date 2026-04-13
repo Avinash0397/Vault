ca00371cc8114dd14f54f5aebf3a138b849c9ee7
Avinash0397 <avimishra478@gmail.com>
2026-04-13 12:39:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -223,38 +223,7 @@ resource "aws_lb" "vault_lb" {
   }
 }
 
-resource "aws_lb_target_group" "vault_tg" {
-  name     = "vault-target-group"
-  port     = 8200
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
 
-  health_check {
-    path                = "/"
-    interval            = 280
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 9
-    matcher             = "200-299"
-  }
-}
-
-resource "aws_lb_target_group_attachment" "vault" {
-  target_group_arn = aws_lb_target_group.vault_tg.arn
-  target_id        = aws_instance.vault.id
-  port             = 8200
-}
-
-resource "aws_lb_listener" "vault" {
-  load_balancer_arn = aws_lb.vault_lb.arn
-  port              = 8200
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.vault_tg.arn
-  }
-}
 
 output "public_instance_dns" {
   value = aws_instance.public_instance.public_dns
