319064a532a440a567d1092767e03df468909b29
Avinash0397 <avimishra478@gmail.com>
2026-04-13 12:34:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -131,7 +131,7 @@ resource "aws_security_group" "public" {
 
 resource "aws_instance" "public_instance" {
   ami                    = "ami-0507f5acd9ba8e6b7"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.public.id] # Fixed: Use vpc_security_group_ids instead of security_groups
   key_name               = "key"
@@ -190,7 +190,7 @@ resource "aws_security_group" "private" {
 
 resource "aws_instance" "vault" {
   ami                    = "ami-0507f5acd9ba8e6b7"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.private.id] # Fixed: Use vpc_security_group_ids instead of security_groups
   key_name               = "key"
@@ -202,7 +202,7 @@ resource "aws_instance" "vault" {
 resource "aws_instance" "consul" {
   count                  = 2
   ami                    = "ami-0507f5acd9ba8e6b7"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.private.id
   key_name               = "key"
   vpc_security_group_ids = [aws_security_group.private.id] # Fixed: Use vpc_security_group_ids instead of security_groups
